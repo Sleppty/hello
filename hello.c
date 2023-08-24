a68cf4ec384e49a604a221abc28792fec5450d9c
Tyler Thomas <tthomas35@husker.unl.edu>
2023-08-24 13:27:24 -0500

    initial commit

--- hello.c
+++ hello.c
@@ -4,5 +4,8 @@ int main(int argc, char **argv) {
     printf("Hello, Tyler Smith\n");
     printf("Your major is Computer Science!\n");
 
+    printf("Hello, Tyler Thomas\n");
+    printf("Your major is Computer Science!\n");
+
     return 0;
 }
